d376624c38b38633ff0425f7f6826938c3eb7d64
Cesare Ferrari <cesare@maryandferrari.com>
2020-05-13 09:29:40 -0400

    Add attributes to room

--- src/room.py
+++ src/room.py
@@ -6,26 +6,10 @@ class Room:
         self.name = name
         self.description = description
         self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def __str__(self):
         return f"Room - name: {self.name}, description: {self.description}, n_to: {self.n_to}"
 
-    # def n_to(self, new_value):
-    #     self.n_to = new_value
-    #     return n_to
-
-
-
-
-# n_to, s_to, e_to, and w_to
-
-
-room1 = Room("Outside Cave Entrance", "North of you, the cave mount beckons")
-room2 = Room("Foyer", "Some description")
-
-print(room1)
-print("\nroom1.n_to", room1.n_to)
-
-room1.n_to = room2
-
-print("\nroom1", room1)
